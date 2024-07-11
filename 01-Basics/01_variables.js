31465e28b8742abe867801afa80066747d31f72c
Rizwan Manzoor <rizwanmanzoor200@gmail.com>
2024-07-11 11:55:51 +0000

    basic variables define

---
+++ 01-Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "admin@gmailcom"
+var accountPassword = "12345"
+accountCity = "lahore"
+let accountStatus;
+
+// accountId = 123
+
+accountEmail = "account@web.com"
+accountPassword = "123"
+accountCity = "karachi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus]);
