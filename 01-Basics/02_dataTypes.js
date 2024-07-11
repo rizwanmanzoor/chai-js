de40c8ff05ecd9a6ca3ab99dce6ce894cfe9f631
Rizwan Manzoor <rizwanmanzoor200@gmail.com>
2024-07-11 12:24:51 +0000

    data types declaration

---
+++ 01-Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as a newer version
+
+/*
+
+*- Data Types (PRIMTIVE)
+Number => 2 to power 53
+string => ""
+boolean => true/false
+null => standalone value (let state = null;)
+undefined => not defined (let state;)
+symbol => unique
+
+*-typeof() //define datatype of a variable
+
+*- Object
+
+
+*/
+
+const accountId = 12345
+let accountPassword = "12345"
+let accountStatus
+let accountValue = null
+
+console.log(typeof(accountId));
+console.log(typeof(accountPassword));
+console.log(typeof(accountStatus));
+console.log(typeof(accountValue));
+
+console.log(typeof(null)); // object
+console.log(typeof(undefined)); // undefined
